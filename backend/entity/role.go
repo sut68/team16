4695cc9a01e2b6fa2ed6a4b7285a01a7e70f275c
Nuy-aissarapab <naissarapab@gmail.com>
2025-12-04 18:11:20 +0700

    สร้าง contoller สำหรับระบบย่อยจัดการข้อมูลผู้ใช้ - close #104

--- backend/entity/role.go
+++ backend/entity/role.go
@@ -4,8 +4,7 @@ import "gorm.io/gorm"
 
 type Role struct {
 	gorm.Model
-	RoleName string `gorm:"unique;not null" json:"role_name"`
-
-	// 1 Role มี User ได้หลายคน (One-to-Many)
-	Users []User `gorm:"foreignKey:RoleID" json:"users"` 
+	Name string `gorm:"unique;not null" json:"name"`
+	
+	Users []User `gorm:"foreignKey:RoleID" json:"users,omitempty"` 
 }
