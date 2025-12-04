c9b7966ba3770067903572379490eb3d00cc08e8
Nuy-aissarapab <naissarapab@gmail.com>
2025-12-04 15:47:27 +0700

    Entity family_info - close #96

---
+++ backend/entity/family_info.go
@@ -0,0 +1,23 @@
+package entity
+
+import "gorm.io/gorm"
+
+type FamilyInfo struct {
+	gorm.Model
+	FatherName         string  `json:"father_name"`
+	FatherOccupation   string  `json:"father_occupation"`
+	FatherIncome       float64 `gorm:"type:decimal(10,2)" json:"father_income"`
+	
+	MotherName         string  `json:"mother_name"`
+	MotherIncome       float64 `gorm:"type:decimal(10,2)" json:"mother_income"`
+	MotherOccupation   string  `json:"mother_occupation"`
+	
+	GuardianName       string  `json:"guardian_name"`
+	GuardianOccupation string  `json:"guardian_occupation"`
+	GuardianIncome     float64 `gorm:"type:decimal(10,2)" json:"guardian_income"`
+	GuardianRelation   string  `json:"guardian_relation"`
+
+	// Foreign Key
+	ProfileID uint           `json:"profile_id"`
+	Profile   StudentProfile `gorm:"foreignKey:ProfileID" json:"student_profile"`
+}
