1c9809c7d8bf2fde519230801ec021896edceb02
10xkz <10kaslongas2912@gmail.com>
2025-12-06 13:34:50 +0700

    สร้าง validation สำหรับ approval_decision - close #25

--- backend/entity/application.go
+++ backend/entity/application.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Application struct {
 	gorm.Model
 
-	StudentProfileID uint           `json:"student_profile_id" valid:"required~Student profile is required"`
+	StudentProfileID uint           `json:"student_profile_id" valid:"required~Student profile ID is required"`
 	StudentProfile   *StudentProfile `gorm:"foreignKey:StudentProfileID" json:"student_profile"`
 
 	ApplicationDocuments []ApplicationDocument `gorm:"foreignKey:ApplicationID" json:"application_documents"`
--- backend/entity/application_document.go
+++ backend/entity/application_document.go
@@ -7,10 +7,10 @@ type ApplicationDocument struct {
 	FileName   string `gorm:"not null" json:"file_name" valid:"required~File name is required"`
 	UploadedBy string `gorm:"not null" json:"uploaded_by" valid:"required~Uploaded by is required"`
 
-	ApplicationID uint        `json:"application_id"`
+	ApplicationID uint        `json:"application_id" valid:"required~Application ID is required"`
 	Application   Application `gorm:"foreignKey:ApplicationID" json:"application"`
 
-	RequirementID       uint                `json:"requirement_id"`
+	RequirementID       uint                `json:"requirement_id" valid:"required~Requirement ID is required"`
 	ApprovalRequirement ApprovalRequirement `gorm:"foreignKey:RequirementID" json:"approval_requirement"`
 
 	ApprovalTasks []ApprovalTask `gorm:"foreignKey:DocumentID" json:"approval_tasks"`
