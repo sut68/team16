b284b22c25b6c41cf8739194bf5b4e02eaa19fde
10xkz <10kaslongas2912@gmail.com>
2025-12-13 14:43:46 +0700

    สร้าง entity interviewer - close #150

---
+++ backend/entity/interviewer.go
@@ -0,0 +1,10 @@
+package entity
+
+import "gorm.io/gorm"
+
+type Interviewer struct {
+	gorm.Model
+	InterviewerFirstname string `gorm:"not null" json:"interviewer_firstname"`
+	InterviewerLastname  string `gorm:"not null" json:"interviewer_lastname"`
+	Email                string `gorm:"not null" json:"email"`
+}
