0bc11e7c214fcc3559df592fbe45b91826eb6e21
Snxxze <keng3772@gmail.com>
2025-12-14 21:51:23 +0700

    Test(sponsor): add unit tests for sponsor and sponsor contact validation

---
+++ backend/test/sponsor_contact_validation_test.go
@@ -0,0 +1,158 @@
+package test
+
+import (
+	"strings"
+	"testing"
+
+	"backend/entity"
+	"backend/validators"
+
+	. "github.com/onsi/gomega"
+)
+
+// case ปกติ
+func TestSponsorContactValidation_AllValid(t *testing.T) {
+	g := NewWithT(t)
+
+	contact := entity.SponsorContact{
+		Name:  "Somchai Rattanakul",
+		Email: "somchai@test.com",
+		Phone: "0812345678",
+	}
+
+	err := validators.ValidateStruct(&contact)
+
+	// ผ่าน
+	g.Expect(err).To(BeNil())
+}
+
+// case Name ว่าง
+func TestSponsorContactValidation_NameRequired(t *testing.T) {
+	g := NewWithT(t)
+
+	contact := entity.SponsorContact{
+		Name:  "",
+		Email: "somchai@test.com",
+		Phone: "0812345678",
+	}
+
+	err := validators.ValidateStruct(&contact)
+
+	// ไม่ผ่าน
+	g.Expect(err).ToNot(BeNil())
+}
+
+// case Name ไม่ถึง 2
+func TestSponsorContactValidation_NameTooShort(t *testing.T) {
+	g := NewWithT(t)
+
+	contact := entity.SponsorContact{
+		Name:  "A",
+		Email: "somchai@test.com",
+		Phone: "0812345678",
+	}
+
+	err := validators.ValidateStruct(&contact)
+
+	// ไม่ผ่าน
+	g.Expect(err).ToNot(BeNil())
+}
+
+// case Email ผิด format
+func TestSponsorContactValidation_InvalidEmail(t *testing.T) {
+	g := NewWithT(t)
+
+	contact := entity.SponsorContact{
+		Name:  "Somchai",
+		Email: "invalid-email",
+		Phone: "0812345678",
+	}
+
+	err := validators.ValidateStruct(&contact)
+
+	// ไม่ผ่าน
+	g.Expect(err).ToNot(BeNil())
+}
+
+// case Phone มีตัวอักษร
+func TestSponsorContactValidation_PhoneNotNumeric(t *testing.T) {
+	g := NewWithT(t)
+
+	contact := entity.SponsorContact{
+		Name:  "Somchai",
+		Email: "somchai@test.com",
+		Phone: "08A123456",
+	}
+
+	err := validators.ValidateStruct(&contact)
+
+	// ไม่ผ่าน
+	g.Expect(err).ToNot(BeNil())
+}
+
+// case Phone ไม่ถึง 9
+func TestSponsorContactValidation_PhoneTooShort(t *testing.T) {
+	g := NewWithT(t)
+
+	contact := entity.SponsorContact{
+		Name:  "Somchai",
+		Email: "somchai@test.com",
+		Phone: "08123456",
+	}
+
+	err := validators.ValidateStruct(&contact)
+
+	// ไม่ผ่าน
+	g.Expect(err).ToNot(BeNil())
+}
+
+// case Phone เกิน 10
+func TestSponsorContactValidation_PhoneTooLong(t *testing.T) {
+	g := NewWithT(t)
+
+	contact := entity.SponsorContact{
+		Name:  "Somchai",
+		Email: "somchai@test.com",
+		Phone: "081234567890",
+	}
+
+	err := validators.ValidateStruct(&contact)
+
+	// ไม่ผ่าน
+	g.Expect(err).ToNot(BeNil())
+}
+
+// case Position เป็น nil หรือ ไม่ส่ง
+func TestSponsorContactValidation_PositionOptional(t *testing.T) {
+	g := NewWithT(t)
+
+	contact := entity.SponsorContact{
+		Name: "Somchai",
+		Email: "somchai@test.com",
+		Phone: "0812345678",
+	}
+
+	err := validators.ValidateStruct(&contact)
+
+	// ผ่าน
+	g.Expect(err).To(BeNil())
+}
+
+// case Position เกิน 50
+func TestSponsorContactValidation_PositionTooLong(t *testing.T) {
+	g := NewWithT(t)
+
+	longPos := strings.Repeat("P", 51)
+
+	contact := entity.SponsorContact{
+		Name:     "Somchai",
+		Email:    "somchai@test.com",
+		Phone:    "0812345678",
+		Position: &longPos,
+	}
+
+	err := validators.ValidateStruct(&contact)
+
+	// ไม่ผ่าน
+	g.Expect(err).ToNot(BeNil())
+}
---
+++ backend/test/sponsor_validation_test.go
@@ -0,0 +1,176 @@
+package test
+
+import (
+	"backend/entity"
+	"backend/validators"
+	"strings"
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+// case ปกติ
+func TestSponsorValidation_AllValid(t *testing.T) {
+	g := NewWithT(t)
+
+	website := "https://www.pttplc.com"
+	desc := "Scholarship sponsor"
+
+	sponsor := entity.Sponsor {
+		CompanyName: "PTT Public Company Limited",
+		Website:     &website,
+		Status:      "active",
+		Description: &desc,
+	}
+
+	err := validators.ValidateStruct(&sponsor)
+
+	// ผ่าน
+	g.Expect(err).To(BeNil())
+}
+
+// case CompanyName ว่าง
+func TestSponsorValidation_CompanyNameEmpty(t *testing.T) {
+	g := NewWithT(t)
+
+	sponsor := entity.Sponsor{
+		CompanyName: "",
+		Status:      "active",
+	}
+
+	err := validators.ValidateStruct(&sponsor)
+
+	// ไม่ผ่าน
+	g.Expect(err).ToNot(BeNil())
+}
+
+// case CompanyName สั้นเกิน
+func TestSponsorValidation_CompanyNameTooShort(t *testing.T) {
+	g := NewWithT(t)
+
+	sponsor := entity.Sponsor{
+		CompanyName: "A",
+		Status:      "active",
+	}
+
+	err := validators.ValidateStruct(&sponsor)
+
+	// ไม่ผ่าน
+	g.Expect(err).ToNot(BeNil())
+}
+
+// case CompanyName ยาวเกิน 100 ตัว
+func TestSponsorValidation_CompanyNameTooLong(t *testing.T) {
+	g := NewWithT(t)
+
+	longName := strings.Repeat("A", 101)
+
+	sponsor := entity.Sponsor{
+		CompanyName: longName,
+		Status:      "active",
+	}
+
+	err := validators.ValidateStruct(&sponsor)
+
+	// ไม่ผ่าน
+	g.Expect(err).ToNot(BeNil())
+}
+
+// case Status inactive
+func TestSponsorValidation_StatusInactiveValid(t *testing.T) {
+	g := NewWithT(t)
+
+	sponsor := entity.Sponsor{
+		CompanyName: "Valid Company",
+		Status:      "inactive",
+	}
+
+	err := validators.ValidateStruct(&sponsor)
+
+	// ผ่าน
+	g.Expect(err).To(BeNil())
+}
+
+// case Status ที่ไม่มี
+func TestSponsorValidation_StatusInvalid(t *testing.T) {
+	g := NewWithT(t)
+
+	sponsor := entity.Sponsor{
+		CompanyName: "Valid Company",
+		Status:      "deleted",
+	}
+
+	err := validators.ValidateStruct(&sponsor)
+
+	// ไม่ผ่าน
+	g.Expect(err).ToNot(BeNil())
+}
+
+// case Website ไม่ใช่ URL
+func TestSponsorValidation_InvalidWebsite(t *testing.T) {
+	g := NewWithT(t)
+
+	website := "not-a-url"
+
+	sponsor := entity.Sponsor{
+		CompanyName: "Valid Company",
+		Status:      "active",
+		Website:     &website,
+	}
+
+	err := validators.ValidateStruct(&sponsor)
+
+	// ไม่ผ่าน
+	g.Expect(err).ToNot(BeNil())
+}
+
+// case ไม่ส่ง Website
+func TestSponsorValidation_WebsiteOptional(t *testing.T) {
+	g := NewWithT(t)
+
+	sponsor := entity.Sponsor{
+		CompanyName: "Valid Company",
+		Status:      "active",
+	}
+
+	err := validators.ValidateStruct(&sponsor)
+
+	// ผ่าน
+	g.Expect(err).To(BeNil())
+}
+
+// case Description ไม่เกิน 500
+func TestSponsorValidation_DescriptionValid(t *testing.T) {
+	g := NewWithT(t)
+
+	desc := "OK"
+
+	sponsor := entity.Sponsor{
+		CompanyName: "Valid Company",
+		Status:      "active",
+		Description: &desc,
+	}
+
+	err := validators.ValidateStruct(&sponsor)
+
+	// ผ่าน
+	g.Expect(err).To(BeNil())
+}
+
+// case Description ยาวเกิน 500
+func TestSponsorValidation_DescriptionTooLong(t *testing.T) {
+	g := NewWithT(t)
+
+	longDesc := strings.Repeat("D", 501)
+
+	sponsor := entity.Sponsor{
+		CompanyName: "Valid Company",
+		Status:      "active",
+		Description: &longDesc,
+	}
+
+	err := validators.ValidateStruct(&sponsor)
+
+	// ไม่ผ่าน
+	g.Expect(err).ToNot(BeNil())
+}
