37e4f145bfebe7dce1819b14815eaebd7952cbc4
Snxxze <keng3772@gmail.com>
2025-12-16 17:03:55 +0700

    Sponsor: Use standardized HTTP helpers

--- frontend/src/services/sponsor/industry.ts
+++ frontend/src/services/sponsor/industry.ts
@@ -1,14 +1,9 @@
-import axios from "axios";
 import type { IndustryResponse } from "../../interfaces/sponsor";
-
-const api = axios.create({
-  baseURL: "http://localhost:8080/api",
-  timeout: 10000,
-})
+import { Get } from "../api";
 
 export const IndustryService = {
-  async getAll(): Promise<IndustryResponse[]> {
-    const res = await api.get<IndustryResponse[]>('/industries');
-    return res.data;
-  },
+  async getAll(requireAuth: boolean = true): Promise<IndustryResponse[]> {
+    const data: IndustryResponse[] = await Get("/industries", requireAuth);
+    return data;
+  }
 };
