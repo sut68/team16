26af330a91f3794cd596fd21909f67c5a692b5ca
tawan-thanyakorn <sahakorn.stn@gmail.com>
2026-01-07 20:18:07 +0700

    unit test assistance - close #276

--- backend/entity/assistance.go
+++ backend/entity/assistance.go
@@ -6,12 +6,12 @@ import (
 
 type Assistance struct {
 	gorm.Model
-	Massage string `json:"massage"`
+	Massage string `json:"massage" valid:"required~massage is required"`
 	
-	ChatroomID uint `json:"chatroom_id"`
+	ChatroomID uint `json:"chatroom_id" valid:"required~chatroom is required"`
 	Chatroom Chatroom `gorm:"foreignKey:ChatroomID" json:"chatroom"`
 	
-	SenderID uint `json:"sender_id"`
+	SenderID uint `json:"sender_id" valid:"required~sender is required"`
 	Sender User	`gorm:"foreignKey:SenderID" json:"sender"`
 	
 }
